846874cab8c9e6016735c43167a3fa52b993a728
Cate Uch <cat.uch@yandex.ru>
2021-11-12 02:40:07 +0300

    getting meetups from database

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,9 +1,11 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
+import { MongoClient, ObjectId } from "mongodb";
+
 
 export default function MeetupDetails(props) {
 
   return (
-    <MeetupDetail id= {props.meetupId.id}
+    <MeetupDetail
     image = {props.meetupId.image}
     title = {props.meetupId.title}
     description = {props.meetupId.description}
@@ -13,28 +15,52 @@ export default function MeetupDetails(props) {
   )
 }
 export async function getStaticPaths() {
+
+    const client = await MongoClient.connect("mongodb+srv://kateuch:femOFMMt9H4pJSf8@cluster0.7j1hg.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollections = db.collection("meetups");
+
+    const meetups = await  meetupsCollections.find({}, {_id: 1}).toArray();
+
+    client.close();
+
     return {
         fallback: false,
-        paths: [
-            {params: {
-                meetupId: 'm1'
-            }}
-        ]
-    }
+        paths: meetups.map(meetup => ({
+            params: {
+                meetupId: meetup.id.toString()
+        }
+    })),
+}
 }
 
-export async function getStaticProps (context) {
-    const meetupId = context.params.meetupId;
 
-    return {
-        props: {
-            meetupId: {
-                id: meetupId,
-                image:'img',
-                title:'tiiel',
-                description:'description',
-                adress:'adress'
-            }
-        }
-    }
+export async function getStaticProps (context) {
+    const client = await MongoClient.connect("mongodb+srv://kateuch:femOFMMt9H4pJSf8@cluster0.7j1hg.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollections = db.collection("meetups");
+
+  const meetupId = context.params.meetupId;
+
+  const selectedMeetups= await  meetupsCollections.findOne({_id: ObjectId(meetupId)});
+
+  client.close();
+
+
+
+  return {
+      props: {
+          meetupId: {
+           id: selectedMeetups._id.toString(),
+           title: selectedMeetups.title,
+           image: selectedMeetups.image,
+           adress: selectedMeetups.adress,
+           description: selectedMeetups.description
+          }
+      }
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,50 @@
 import MeetupList from "../components/meetups/MeetupList";
+import { MongoClient} from "mongodb";
+
 
 const Img = require("../public/default.jpg");
 
-const initialMeetupList = [
-  {
-    id: "m1",
-    image: Img,
-    title: "React Vienna",
-    address: "Vienna, Austria",
-    description: `We love React and we thought it would be a great idea to put together a place where people can talk, meet and share their knowledge to bring their skills to the next level. Let's get better at doing this, together! All our HackJam events are free and formatted as hands-on workshops, so don't hesitate: grab your computer and join us!`,
-  },
-  {
-    id: "m2",
-    image: Img,
-    title: "React VIX",
-    address: "Vila Velha, Brasil",
-    description:
-      "Oi!Se você é amante de tecnologia, desenvolve software, trabalha com design, empreende, inova ou está procurando alguma coisa interessante pra fazer no ES, vamos trocar uma ideia?",
-  },
-];
+// const initialMeetupList = [
+//   {
+//     id: "m1",
+//     image: Img,
+//     title: "React Vienna",
+//     address: "Vienna, Austria",
+//     description: `We love React and we thought it would be a great idea to put together a place where people can talk, meet and share their knowledge to bring their skills to the next level. Let's get better at doing this, together! All our HackJam events are free and formatted as hands-on workshops, so don't hesitate: grab your computer and join us!`,
+//   },
+//   {
+//     id: "m2",
+//     image: Img,
+//     title: "React VIX",
+//     address: "Vila Velha, Brasil",
+//     description:
+//       "Oi!Se você é amante de tecnologia, desenvolve software, trabalha com design, empreende, inova ou está procurando alguma coisa interessante pra fazer no ES, vamos trocar uma ideia?",
+//   },
+// ];
 
 export default function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
+  const client = await MongoClient.connect("mongodb+srv://kateuch:femOFMMt9H4pJSf8@cluster0.7j1hg.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollections = db.collection("meetups");
+
+  const meetups= await  meetupsCollections.find().toArray();
+
+  client.close();
+
   return {
-    props: {
-      meetups: initialMeetupList,
-    },
-  };
+      props: {
+        meetups:   meetups.map((meetup) => ({
+          title: meetup.title,
+          address: meetup.address,
+          image: meetup.image,
+          id: meetup._id.toString(),
+        }))
+  }
+}
 }
